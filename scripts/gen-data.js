1ff6c7af71357f6af95d210c09d0899fb0fd80bf
Thiago Mundici De Sant'Anna <thiago.santanna@sensedia.com>
2024-11-06 18:02:15 -0300

    feat: Adding comments explaining the code

--- scripts/gen-data.js
+++ scripts/gen-data.js
@@ -1,6 +1,9 @@
-const randomNumber = () => Math.round(Math.random() * 1000);
-const VIDEO_CATEGORIES = ["music", "sports", "gaming"];
+// Esse script depende da importação do Faker.js no HTML utilizado
 
+const randomNumber = () => Math.round(Math.random() * 1000); // Gera um número aleatório entre 0 e 1000
+const VIDEO_CATEGORIES = ["music", "sports", "gaming"]; // Cosntante das categorias de vídeos
+
+// Gera um array de 50 objetos com as propriedades title, thumbnail, channelName e category (video)
 const VIDEOS = [...Array(50)].map(() => ({
   title: faker.company.bs(),
   thumbnail: `${faker.image.lorempicsum.imageUrl(1280, 720)}?${randomNumber()}`,
@@ -8,14 +11,20 @@ const VIDEOS = [...Array(50)].map(() => ({
   category: faker.helpers.randomize(VIDEO_CATEGORIES),
 }));
 
+// Captura o grid de videos, elemento responsável pela exibição dos vídeos
 const videoGrid = document.querySelector(".video-grid");
 
+// Percorre toda a lista de videos gerada
 for (const video of VIDEOS) {
+  // Cria o card de video no HTML
   const videoCard = createVideoCard(video);
 
+  // Atribui ele no grid de videos
   videoGrid.appendChild(videoCard);
 }
 
+// Função responsável por criar no HTML de forma dinâmica e automatizada
+// o card de video com as informação geradas no array VIDEOS
 function createVideoCard(video) {
   const videoCard = document.createElement("div");
   videoCard.classList.add("video-card");
@@ -35,6 +44,7 @@ function createVideoCard(video) {
   const videoCategory = document.createElement("p");
   videoCategory.textContent = video.category;
 
+  // Atribui as informações do video em elementos children do videoCard
   videoCard.appendChild(videoThumbnail);
   videoCard.appendChild(videoTitle);
   videoCard.appendChild(videoChannelName);
@@ -43,6 +53,7 @@ function createVideoCard(video) {
   return videoCard;
 }
 
+// Exemplo de card de video em HTML
 /* <div class="video-card music">
   <img src="https://i.ytimg.com/vi/SrUgg03-uQY/maxresdefault.jpg" alt="Video Thumbnail">
   <h3>Video Title</h3>
--- scripts/search.js
+++ scripts/search.js
@@ -1,16 +1,20 @@
+// Função chamada no momento em que o usuário digita algo no campo de busca
 function searchVideo(event) {
   const videos = document.querySelectorAll(".video-card");
-  const search = event.target.value.toLowerCase();
+  const search = event.target.value.toLowerCase(); // Valor inserido na barra de busca
 
   for (const video of videos) {
+    // Traduz o card de video no HTML em um objeto
     const videoInfo = {
       title: video.getElementsByTagName("h3")[0].innerHTML.toLowerCase(),
       channelName: video.getElementsByTagName("p")[0].innerHTML.toLowerCase(),
       category: video.getElementsByTagName("p")[1].innerHTML.toLowerCase(),
     };
 
+    // Utilizando do valor "display: revert" nos videos é o que torna possível desfazer a busca
     video.style.display = "revert";
 
+    // Se caso o video não possuir o termino em nenhum dos campos, ele é ocultado
     if (searchThrough(videoInfo, search)) video.style.display = "none";
   }
 }
